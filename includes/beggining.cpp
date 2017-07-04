849ab6b46dbdf4c6ff0dc96cce2b7b0069ec8135
Dan64 <futurenoird@gmail.com>
2017-07-04 22:44:16 +0000

    Subir nuevo archivo

---
+++ includes/beggining.cpp
@@ -0,0 +1,80 @@
+void beginning(){
+	cout << "Te has despertado y te estan atacando...\n";
+	cout << "Un troll a aparecido\n";
+	enemy troll;
+	troll.byDefault();
+	
+	player plr;
+	plr.byDefault();
+	
+		do {
+			int user;
+			cout << "Salud: " << plr.getVida()
+			<< " Magia: " << plr.getMana() << endl;
+			cout << "1. Atacar\n2. Magia\n3. Rendirse\n" << endl;
+			cout << "Introduce accion: ";
+			cin >> user;
+			if(user == 1){//ataque al enemigo
+				if(troll.getVida() > 0){
+					int total = plr.getAtaque() + plr.getFuerza();
+					troll.setVida(-total);
+					cout << "El enemigo ha perdido " << total
+					<< " puntos de salud!\n" << endl;
+					if(troll.getVida() <= 0){
+						cout << "Enemigo abatido!" << endl;
+						break;
+					}
+				}
+			}else if(user == 2){// usar magia contra enemigo
+				if (plr.getMana() > 0){
+					cout << "El enemigo ha perdido " << plr.getMagia()
+					<< " puntos de salud!\n" << endl;
+					plr.setMana(-10);
+					troll.setVida(-plr.getMagia());
+				}else{
+					cout << "No tienes mana suficiente!\n" << endl;
+				}
+			}else if (user == 3){// Rendirse
+				cout << "Has sido prisionero de tu enemigo" << endl;
+				break;
+			}else{cout << "Argumento no valido" << endl;}
+		
+			system("pause");
+			system("cls");
+	
+			//Ataque del enemigo
+			if(troll.getVida() > 0){
+				if(plr.getVida() > 0){
+					int total = troll.getAtaque() + troll.getFuerza();
+					cout << "Enemigo: "<< " Salud: " << troll.getVida() << endl;
+			
+					cout << "El enemigo ataca..." << endl;
+					
+					plr.setVida(-total);
+					cout << "Has perdido " << total << " puntos de salud!\n" << endl;
+					if (plr.getVida() <= 0){
+						cout << "Has caido!\n";
+						system("pause");
+						system("cls");
+						break;
+					}
+					system("pause");
+					system("cls");
+				}else{
+					cout << "Has caido!\n";
+				}
+			}
+	
+	
+	}while(plr.getVida() > 0);
+	
+	system("cls");
+	if(troll.getVida() <= 0){
+		cout << "Has ganado!" << endl;
+	}else{
+		cout << "Te acaban de joder el dia...\n";
+	}
+	
+	cout << "Fin del juego\n" << endl;
+
+}
