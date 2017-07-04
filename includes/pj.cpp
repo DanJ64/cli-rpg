b12e4c8da31e965c4515c3bc5e983dcd177e683e
Dan64 <futurenoird@gmail.com>
2017-07-04 22:46:39 +0000

    Subir nuevo archivo

---
+++ includes/pj.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+void pj(){
+	
+cout << "             . --- ." << endl;
+cout << "           /        \\ " << endl;
+cout << "          |  O  _  O |" << endl;
+cout << "          |  ./   \\. |" << endl;
+cout << "          /  `-._.-'  \\ " << endl;
+cout << "        .' /         \\ `." << endl;
+cout << "    .-~.-~/           \\~-.~-." << endl;
+cout << ".-~ ~    |             |    ~ ~-." << endl;
+cout << "`- .     |             |     . -'" << endl;
+cout << "     ~ - |             | - ~" << endl;
+cout << "         \\            /" << endl;
+cout << "       ___\\           /___" << endl;
+cout << "       ~;_  >- . . -<  _i~" << endl;
+cout << "          `'         `'" << endl;
+}
